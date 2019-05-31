f27416705d914534ab6357f33f7e4ee634f3371a
Domenico Garofoli <d.garofoli@cantierecreativo.net>
2019-05-31 19:30:30 +0200

    Load all files in support directory

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'restcountry'
+
+Dir["./spec/support/**/*.rb"].sort.each { |file| require file }
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
